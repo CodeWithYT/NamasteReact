5f26711d9aa638517c0f16fc8bbe11f5f9b11ea5
Y.T <tarunyelleti@gmail.com>
2025-07-27 20:36:45 +0530

    modified the meanings and added theory

--- inception01/app.js
+++ inception01/app.js
@@ -1,7 +1,7 @@
 const heading = React.createElement(
     "h1",                   // tag to be created
-     {id: "heading", class: "firstCss"},       // we can pass the attributes
-      "hello from react"    // content to be pass to the tag
+     {id: "heading", class: "firstCss"},       // we can pass the attributes (object)
+      "hello from react"    // content to be pass to the tag (children)
     );
 const root = ReactDOM.createRoot(document.getElementById("root"));  // creating the root in the react
 root.render(heading);       // rendering the heading object into the created root
